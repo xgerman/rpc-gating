0a4b4862428f4c04772fdbdd6ba71cd55e6d7242
git-harry <git-harry@live.co.uk>
2017-09-05 12:42:37 +0100

    Troubleshooting initial incorrect values

--- influx-reports/influx-report.sh
+++ influx-reports/influx-report.sh
@@ -10,4 +10,11 @@ pushd "$WORKSPACE"
   pip install -r rpc-gating/requirements.txt -c rpc-gating/constraints.txt
 popd
 mkdir -p "${REPORTS_FOLDER}"
-$INFLUX_DOWNTIME_FOLDER/influx.py --ymlreport $REPORTS_FOLDER/influx-downtime.yml --subunitreport $REPORTS_FOLDER/influx-downtime.bin $ADDITIONAL_ARGS
+x=0
+while [ $x -lt 48 ]
+do
+  echo "Report $x, total elapsed time $(( $x * 5)) minutes"
+  $INFLUX_DOWNTIME_FOLDER/influx.py --ymlreport $REPORTS_FOLDER/influx-downtime.yml --subunitreport $REPORTS_FOLDER/influx-downtime.bin $ADDITIONAL_ARGS
+  x=$(( $x + 1 ))
+  sleep 5m
+done
